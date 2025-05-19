9b8a57522fbcd49438b95260b124134abe8b8111
Itsel Ramirez <itsel.gabriela.ramirez@gmail.com>
2025-05-19 14:48:27 -0600

    actualizacion de validaciones de cliente y producto DTO

--- common-ecommerce-microservicio/src/main/java/com/itsel/common/dto/ClienteDTO.java
+++ common-ecommerce-microservicio/src/main/java/com/itsel/common/dto/ClienteDTO.java
@@ -1,24 +1,97 @@
 package com.itsel.common.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public class ClienteDTO {
 	
 private Long id;
 	
+	@NotNull(message = "El nombre no puede ser nulo")
 	@NotBlank(message = "El nombre no puede estar en blanco")
 	private  String nombre;
 	
+	@NotNull(message = "El apellido no puede ser nulo")
 	@NotBlank(message = "El apellido no puede estar en blanco")
 	private  String apeliido;
 	
-	@NotBlank(message = "El email no puede estar en blanco")
+	@NotNull(message = "El email no puede ser nulo")
+    @NotBlank(message = "El email no puede estar en blanco")
+    @Email(message = "El email debe tener un formato válido (ejemplo@dominio.com)")
 	private  String email;
 	
-	@NotBlank(message = "El telefono no puede estar en blanco")
+	@NotNull(message = "El teléfono no puede ser nulo")
+    @NotBlank(message = "El teléfono no puede estar en blanco")
+    @Pattern(regexp = "^[0-9]{10}$", message = "El teléfono debe tener exactamente 10 dígitos numéricos")
 	private  String telefono;
 	
-	@NotBlank(message = "La direccion no puede estar en blanco")
+	
+	@Size(max = 100, message = "La dirección no debe exceder los 100 caracteres")
 	private  String direccion;
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApeliido() {
+		return apeliido;
+	}
+
+
+	public void setApeliido(String apeliido) {
+		this.apeliido = apeliido;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	
 
 }
--- common-ecommerce-microservicio/src/main/java/com/itsel/common/dto/ProductoDTO.java
+++ common-ecommerce-microservicio/src/main/java/com/itsel/common/dto/ProductoDTO.java
@@ -1,5 +1,70 @@
 package com.itsel.common.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
 public class ProductoDTO {
+	
+	private Long id;
+	
+	@NotNull(message = "El nombre no puede ser nulo")
+	@NotBlank(message = "El nombre no puede estar en blanco")
+	private  String nombre;
+	
+	@NotNull(message = "La descripcion no puede ser nulo")
+	@NotBlank(message = "La descripcion no puede estar en blanco")
+	private  String descripcion;
+	
+	@NotNull(message = "El precio no puede ser nulo")
+    @PositiveOrZero(message = "El precio no puede ser negativo")
+	private  Long precio;
+	
+	@NotNull(message = "El stock no puede ser nulo")
+    @PositiveOrZero(message = "El stock no puede ser negativo")
+	private  Long stock;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Long getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Long precio) {
+		this.precio = precio;
+	}
+
+	public Long getStock() {
+		return stock;
+	}
+
+	public void setStock(Long stock) {
+		this.stock = stock;
+	}
+	
+	
+
 
 }
