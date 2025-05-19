b0aba1f69c69424989e4c4aa694be4ea73600b36
Itsel Ramirez <itsel.gabriela.ramirez@gmail.com>
2025-05-19 14:26:36 -0600

    Avance en cliente DTO

--- common-ecommerce-microservicio/src/main/java/com/itsel/common/dto/ClienteDTO.java
+++ common-ecommerce-microservicio/src/main/java/com/itsel/common/dto/ClienteDTO.java
@@ -1,5 +1,24 @@
 package com.itsel.common.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class ClienteDTO {
+	
+private Long id;
+	
+	@NotBlank(message = "El nombre no puede estar en blanco")
+	private  String nombre;
+	
+	@NotBlank(message = "El apellido no puede estar en blanco")
+	private  String apeliido;
+	
+	@NotBlank(message = "El email no puede estar en blanco")
+	private  String email;
+	
+	@NotBlank(message = "El telefono no puede estar en blanco")
+	private  String telefono;
+	
+	@NotBlank(message = "La direccion no puede estar en blanco")
+	private  String direccion;
 
 }
